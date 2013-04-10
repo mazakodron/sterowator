c076ff5bad1bc89469b610da3b7c0ee508c519ce
Marcin Błaszyk <haldiraros@gmail.com>
2013-04-10 13:00:06 +0200

    Deleted switch class : not needed

--- sterowator.py
+++ sterowator.py
@@ -95,25 +95,6 @@ class Vector2D ():
 	
 	return Vector2D.angle(v0, v1)
 
-class switch(object):
-  def __init__(self, value):
-    self.value = value
-    self.fall = False
-
-  def __iter__(self):
-    """Return the match method once, then stop"""
-    yield self.match
-    raise StopIteration
-    
-  def match(self, *args):
-    """Indicate whether or not to enter a case suite"""
-    if self.fall or not args:
-      return True
-    elif self.value in args: # changed for v1.5, see below
-      self.fall = True
-      return True
-    else:
-      return False
 	  
 def goStep(): # goes one 5.75 deg, step, LEFT motor spins counter-clokwise, RIGHT clockwise
   for x in range(8):
