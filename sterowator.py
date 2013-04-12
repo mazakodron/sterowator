b62b85ffab845d70776face7d5f49bc2d4efb2f0
Sebastian Krzyszkowiak <dos@dosowisko.net>
2013-04-13 01:44:35 +0200

    ETA showing

--- sterowator.py
+++ sterowator.py
@@ -19,9 +19,7 @@ WHEEL_R = 18.0 #promien koła (w milimetrach)
 ROBOT_R = 119.5 #odległosc między pisakiem a kołem - polowa odległosci rozstawu kol (w milimetrach)
 REV_STEP = 1.0/512.0 #obrót osi silnika przy wykonaniu jednej serii kroków (seria 8 kroków)
 
-MOTOR_DELAY = 1.00 #opóźnienie między krokami w milisekundach
-
-DEBUG = len(argv)>2 and argv[2]=='debug'
+MOTOR_DELAY = 1.0 #opóźnienie między krokami w milisekundach
 
 port = parapin.Port(LPT1, outmode=LP_PIN01|LP_DATA_PINS|LP_PIN16|LP_PIN17) # przejęcie obsługi portu i ustawienie pinów w tryb wyjścia
 
@@ -42,14 +40,8 @@ R_4 = port.get_pin(1)
 MAZAK_UP = port.get_pin(16) #mazakowego silnika
 MAZAK_DOWN = port.get_pin(17)
 
-timerCounter = 0
-
-if DEBUG:
-  def msleep(x):
-    global timerCounter
-    timerCounter=timerCounter+x
-else:
-  msleep = lambda x: time.sleep(x/1000.0) #definicja sleep w milisekundach
+msleep = lambda x: time.sleep(x/1000.0) #definicja sleep w milisekundach
+#msleep = lambda x: x
 
 class Vector2D (): #definicja klasy wektorów dwuwymiarowych, potrzebna do oblicznia kątów obrotu robota
   def __init__(self, x, y):
@@ -328,7 +320,7 @@ def clearPins(): #wyczyszczenie wszystkich pinów
   MAZAK_UP.clear()
   MAZAK_DOWN.clear()
   
-def liftMazak(t = 50): #podniesienie mazaka
+def liftMazak(t = 25): #podniesienie mazaka
   MAZAK_DOWN.clear()
   MAZAK_UP.set()
   
@@ -336,7 +328,7 @@ def liftMazak(t = 50): #podniesienie mazaka
   
   MAZAK_UP.clear()
   
-def dropMazak(t = 100): #opuszczenie mazaka
+def dropMazak(t = 50): #opuszczenie mazaka
   MAZAK_UP.clear()
   MAZAK_DOWN.set()
   
@@ -351,31 +343,63 @@ def stepsForRotation(radians): #funkcja zwracająca przybliżoną do całkowitej
 
 def progress(i, lines):
   return int(100*i/lines)
- 
-if __name__ == "__main__":
 
-  print("Mazakodron 3000 - Sterowator 2000")
+def countTime(filename):
 
-  inputfile = ''
-  try:
-    filename = argv[1]
-  except IndexError:
-    raise AssertionError('Podaj plik z danymi')
+  mazak_lifted = False
+  
+  p1 = Vector2D(0.0,0.0)
+  p3 = Vector2D(0.0,-1.0)
+  p2 = Vector2D(0.0,0.0)
+  
+  fd = open(filename,'r')
+  line = fd.readline()
+  i = 1
 
-  with open(filename) as f:
-    for i, l in enumerate(f):
-      pass
-  lines = i  
+  time = 0
+
+  while line:
+    if line.find(' ') != -1:
+      x = float(line[:line.find(' ')])
+      y = float(line[line.find(' ')+1:-1])
+      p2 = Vector2D(x,y)
+
+      pom = Vector2D.angleFromPoints(p1,p2,p3)
+
+      if abs(pom) > math.pi/2:
+        pom -= math.pi/2 * abs(pom)/pom
+        pom *= -1
+        backwards = True
+      else:
+        backwards = False
+
+      st = stepsForRotation(math.fabs(pom))
+
+      time += st * 8 * MOTOR_DELAY
+      time += 64 * MOTOR_DELAY
+      p3 = p1
+      p1 = p2
+    elif line.find('PODNIES') != -1:
+      time += 25
+    elif line.find('OPUSC') != -1:
+      time += 50
+    line = fd.readline()
+    i+=1
+
+  time += 500
+  return time*1.125/1000.0
+
+
+def draw(filename):
 
   p1 = Vector2D(0.0,0.0)
   p3 = Vector2D(0.0,-1.0)
   p2 = Vector2D(0.0,0.0)
-
+  
   dropRequest = False
   
   clearPins()
-  raw_input("Ustaw robota w lewym górnym rogu kartki, podepnij zasilanie i wcisnij ENTER")
-  
+
   fd = open(filename,'r')
   line = fd.readline()
   i = 1
@@ -413,19 +437,23 @@ if __name__ == "__main__":
         x = float(line[:line.find(' ')])
         y = float(line[line.find(' ')+1:-1])
         p2 = Vector2D(x,y)
-      
+
         pom = Vector2D.angleFromPoints(p1,p2,p3)
 
         if abs(pom) > math.pi/2:
           pom -= math.pi/2 * abs(pom)/pom
+          pom *= -1
           backwards = True
         else:
           backwards = False
-        
+
         st = stepsForRotation(math.fabs(pom))
 
-        print("[%3d%%] %s do %s do punktu %.2f %.2f" % (progress(i,lines), 'Jadę' if mazak_lifted else 'Rysuję', 'tyłu' if backwards else 'przodu', x, y))
-        
+        etaLeft = int(eta - time.time() + start_time)
+        if etaLeft < 0:
+          etaLeft = 0
+        print("[%3d%%] %s do %s do punktu %.2f %.2f (ETA: %dm)" % (progress(i,lines), 'Jadę' if mazak_lifted else 'Rysuję', 'tyłu' if backwards else 'przodu', x, y, int(etaLeft/60)))
+
         if st > 0 :
           def prog(i, max):
             print(" - kroków: %d*8=%d, kąt: %.2f (%d%%)" % (st, st*8, pom, int(100*i/max)), end='\r')
@@ -444,14 +472,40 @@ if __name__ == "__main__":
           goStep()
         p3 = p1
         p1 = p2
-      
+
       line = fd.readline()
       i+=1
-    
+
   except KeyboardInterrupt:
     print("Clearing pins...                                ")
     liftMazak(100)
+    clearPins()
+    raise KeyboardInterrupt
 
-clearPins()
-time_elapsed = int(time.time() - start_time + timerCounter/1000)
-print("Time elapsed: %dm%s%ds" % (int(time_elapsed/60), '0' if time_elapsed%60<10 else '', time_elapsed%60))
+  clearPins()
+  time_elapsed = int(time.time() - start_time)
+  print("Time elapsed: %dm%s%ds" % (int(time_elapsed/60), '0' if time_elapsed%60<10 else '', time_elapsed%60))
+  return time_elapsed
+ 
+if __name__ == "__main__":
+
+  print("Mazakodron 3000 - Sterowator 2000")
+
+  inputfile = ''
+  try:
+    filename = argv[1]
+  except IndexError:
+    raise AssertionError('Podaj plik z danymi')
+
+  with open(filename) as f:
+    for i, l in enumerate(f):
+      pass
+  lines = i  
+
+  eta = countTime(filename)
+  print("Plik %s, ETA: %dm%s%ds" % (filename, int(eta/60), '0' if eta%60<10 else '', eta%60))
+  
+  clearPins()
+  raw_input("Ustaw robota w lewym górnym rogu kartki, podepnij zasilanie i wcisnij ENTER")
+  
+  draw(filename)
